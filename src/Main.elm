560c8061e91ad9e5d5223a801e6d942b97334aee
Ale Paredes <alexandra@codeclimate.com>
2017-02-26 21:52:41 -0500

    Add draw and flip card

--- src/Main.elm
+++ src/Main.elm
@@ -3,7 +3,7 @@ module Main exposing (..)
 import Html exposing (Html, button, div, input, text)
 import Html.Events exposing (onClick, onInput)
 import Html.Attributes exposing (class, placeholder)
-import String
+import List
 
 
 main =
@@ -14,10 +14,17 @@ main =
 -- MODEL
 
 
+type alias Card =
+    { id : String
+    , value : String
+    , flipped : Bool
+    }
+
+
 type alias Model =
     { player : String
     , counter : Int
-    , cards : List String
+    , cards : List Card
     }
 
 
@@ -33,6 +40,7 @@ model =
 type Msg
     = UpdatePlayer String
     | ShuffleCards
+    | FlipCard Card
 
 
 update : Msg -> Model -> Model
@@ -42,7 +50,29 @@ update msg model =
             { model | player = newPlayer }
 
         ShuffleCards ->
-            { model | cards = [ "A", "C", "B", "A", "B", "C" ] }
+            { model
+                | cards =
+                    List.map (\( id, value ) -> (Card id value False))
+                        [ ( "1", "A" ), ( "2", "C" ), ( "3", "B" ), ( "4", "A" ), ( "5", "B" ), ( "6", "C" ) ]
+            }
+
+        FlipCard selectedCard ->
+            let
+                updateCard card =
+                    if card.id == selectedCard.id then
+                        { card | flipped = not card.flipped }
+                    else
+                        card
+            in
+                { model | cards = (List.map updateCard model.cards) }
+
+
+drawCard : Card -> Html Msg
+drawCard card =
+    if card.flipped then
+        div [ onClick (FlipCard card) ] [ text card.value ]
+    else
+        div [ onClick (FlipCard card) ] [ text "card" ]
 
 
 
@@ -55,5 +85,5 @@ view model =
         [ input [ placeholder "Player name", onInput UpdatePlayer ] []
         , div [] [ text model.player ]
         , button [ onClick ShuffleCards ] [ text "Shuffle cards" ]
-        , div [] [ text (String.join ", " model.cards) ]
+        , div [] (List.map drawCard model.cards)
         ]
